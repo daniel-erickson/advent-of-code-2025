a0593ad9bfb0a811cf8e36070b3437b295ac8e6c
Daniel Erickson <daniel.erickson@propelleraero.com.au>
2025-12-09 12:44:37 +1100

    get day8 and day9 ready

---
+++ day8/src/main.rs
@@ -0,0 +1,51 @@
+use std::time::Instant;
+use utils::read_input;
+
+fn solve_part1(input: &str) -> i64 {
+    0
+}
+
+fn solve_part2(input: &str) -> i64 {
+    0
+}
+
+fn solve(input: &str) -> (i64, i64) {
+    let part1 = solve_part1(input);
+    let part2 = solve_part2(input);
+    (part1, part2)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let start = Instant::now();
+    println!("Loading input from ./inputs/day8.txt");
+
+    let input = read_input("./inputs/day8.txt")?;
+    let (part1, part2) = solve(&input);
+
+    println!("Part 1: {}", part1);
+    println!("Part 2: {}", part2);
+
+    let duration = start.elapsed();
+    println!("Execution time: {:?}", duration);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = r#""#;
+
+    #[test]
+    fn test_part1() {
+        let result = solve_part1(TEST_INPUT);
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        let result = solve_part2(TEST_INPUT);
+        assert_eq!(result, 0);
+    }
+}
